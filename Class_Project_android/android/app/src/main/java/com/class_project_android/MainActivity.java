a173449e81c4967398fac1d4597cc4a258418d7d
Ember Fairbanks <ember.fairbanks@aggiemail.usu.edu>
2018-03-23 13:56:29 -0600

    Initial Commit

---
+++ Class_Project_android/android/app/src/main/java/com/class_project_android/MainActivity.java
@@ -0,0 +1,15 @@
+package com.class_project_android;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "Class_Project_android";
+    }
+}
---
+++ Class_Project_android/src/main.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import App from './components/App';
+
+AppRegistry.registerComponent('Class_Project_android', () => App);
